b4c6bdc0f55176caa744447b55946078dae8a3c3
yob-yob <mr.seangrover@icloud.com>
2021-07-31 19:03:09 +0800

    Update Dotfiles to my preferences.

--- install.sh
+++ install.sh
@@ -49,7 +49,5 @@ ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
 # Symlink the Mackup config file to the home directory
 ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
-ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
-
 # Set macOS preferences - we will run this last because this will reload the shell
 source .macos
